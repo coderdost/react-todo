e42bd850eacd6a40683e1c7313b64f79838dd1d9
AbhisheK Rathore <rathore.abhishek87@gmail.com>
2021-05-12 17:14:57 +0530

    firebase firestore document based queries

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 
-function App({db}) {
+function App({ db }) {
 
-  useEffect(()=>{
-    db.collection('tasks').doc('first').get().then((doc)=>{
-      console.log(doc.data());
-      setTasks(doc.data().tasks)
-    })
-  },[])
-  
+
   const [task, setTask] = useState({ name: '', completed: false });
   const [tasks, setTasks] = useState([]);
+  const [sortOption, setSortOption] = useState('time')
+
+
+  useEffect(() => {
+    db.collection('tasks').orderBy(sortOption)
+      .get()
+      .then((querySnapshot) => {
+          const allTasks = []
+          querySnapshot.forEach((doc)=>{
+            allTasks.push(doc.data())
+          })
+          if(allTasks.length){
+            setTasks(allTasks);
+          }
+      });
+  }, [sortOption]);
+
+  const sortTask =(option)=>{
+   setSortOption(option);
+  }
+  
   const getTasks = () => {
     return tasks.map((task, index) => (
       <li
@@ -33,31 +48,58 @@ function App({db}) {
       </li>
     ));
   };
-const storeTasks = (tasks)=>{
-  setTasks(tasks);
-  db.collection('tasks').doc('first').set({
-    tasks : tasks
-  })
-}
+  // const storeTasks = (tasks) => {
+  //   setTasks(tasks);
+  //   db.collection('tasks').doc('first').set({
+  //     tasks: tasks,
+  //   });
+  // };
+
+  const addTasksToFirebase = (task)=>{
+    const taskRef = db.collection('tasks').doc();
+    const newTask = {...task, id: taskRef.id, time: new Date()};
+    taskRef.set(newTask).then(()=>{
+      setTasks([...tasks,newTask]);
+    })
+  }
+
+  const deleteTaskFromFirebase = (task)=>{
+   
+    const taskRef = db.collection('tasks').doc(task.id);
+
+    taskRef.delete().then(()=>{
+      console.log('deleted', task.id);
+      setTasks(tasks.filter(t=> t.id!==task.id));
+    })
+  }
+
+  const updateTaskFromFirebase = (task,i)=>{
+   
+    const taskRef = db.collection('tasks').doc(task.id);
+
+    taskRef.update(task).then(()=>{
+      console.log('updated', task.id);
+      const allTasks = [...tasks]
+      allTasks.splice(i,1,task);
+      setTasks(allTasks);
+    })
+  }
+
 
   const updateTask = (i) => {
-    const newTasks = [...tasks];
-    newTasks.splice(i, 1, {
-      name: newTasks[i].name,
-      completed: !newTasks[i].completed,
-    });
-    storeTasks(newTasks);
+    const updatedTask = {...tasks[i], completed: !tasks[i].completed}
+    updateTaskFromFirebase(updatedTask,i);
+
   };
   const deleteTask = (i) => {
-    const newTasks = [...tasks];
-    newTasks.splice(i, 1);
-    storeTasks(newTasks);
+    
+    deleteTaskFromFirebase(tasks[i]);
   };
   const addTask = (t) => {
     if (t) {
       const newTasks = [...tasks];
-      newTasks.push({ name: t, completed: false });
-      storeTasks(newTasks);
+      addTasksToFirebase({ name: t, completed: false });
+      // storeTasks(newTasks);
       setTask({ name: '', completed: false });
     } else {
       alert('enter valid value');
@@ -82,6 +124,23 @@ const storeTasks = (tasks)=>{
         }}
       >
         Change
+      </button>
+      <br></br>
+      <button
+        className="btn btn-warning"
+        onClick={() => {
+          sortTask('name');
+        }}
+      >
+        Sort by Name
+      </button>
+      <button
+        className="btn btn-warning"
+        onClick={() => {
+          sortTask('completed');
+        }}
+      >
+        Sort by Completed
       </button>
       <ul className="list-group">{getTasks()}</ul>
     </div>
